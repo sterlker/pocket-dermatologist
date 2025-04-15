1a1ed6a62e249805f9dca0a985127beb01f762ee
sterlker <151909596+sterlker@users.noreply.github.com>
2025-04-15 12:05:39 +0800

    Update Config.java

--- app/src/main/java/com/example/fypcsp650/Config.java
+++ app/src/main/java/com/example/fypcsp650/Config.java
@@ -1,5 +1,5 @@
 package com.example.fypcsp650;
 
 public class Config {
-    public static final String BASE_URL = "http://10.0.171.125/SkinDiseaseDB/";
+    public static final String BASE_URL = "http://IP/SkinDiseaseDB/";
 }
